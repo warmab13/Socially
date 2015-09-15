0b0d072605d27a572698dcf56e55733b5a163392
Alonso S <alonsosalcido13@gmail.com>
2015-09-15 12:20:17 -0600

    Finish of the third lesson from angular meteor

---
+++ app.js
@@ -0,0 +1,23 @@
+Parties = new Mongo.Collection("parties");
+if (Meteor.isClient) {
+  
+  angular.module('socially', ['angular-meteor']);
+      angular.module('socially').controller('PartiesListCtrl', ['$scope', '$meteor', function ($scope, $meteor) {
+    $scope.parties = $meteor.collection(Parties);
+  }]);
+}
+
+if(Meteor.isServer){
+    Meteor.startup(function(){
+        if(Parties.find().count() == 0){
+            var parties = [
+                {'name':'Dubstep-Free Zone', 'Description':'Fast just got faster wit Nexus S.'},
+                {'name':'All dubstep all the time', 'Description':'Get it on!'},
+                {'name':'Savage loungin', 'Description':'Leisure suit required. And only the fiercest manners.'}
+            ];
+            
+        for(var i = 0; i<parties.length; i++)
+            Parties.insert(parties[i]);
+        }
+    });
+}
